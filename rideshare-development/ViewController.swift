8d60df4f81f6b973d8a6915d9022188f21c0d372
Ayush Munot <amunot6@gatech.edu>
2019-06-24 13:51:30 -0400

    Initial Commit

---
+++ rideshare-development/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rideshare-development
+//
+//  Created by Ayush Munot on 24/06/19.
+//  Copyright © 2019 Ayush Munot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
